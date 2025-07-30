9c9637b0b4832580ad486e262b826cc7cd74f614
Mustafa Shoaib <152904440+muhammadmustafashoaib@users.noreply.github.com>
2025-07-30 23:00:09 +0500

    updated dashboard

--- src/app/Components/invest-in-steps/invest-in-steps.component.ts
+++ src/app/Components/invest-in-steps/invest-in-steps.component.ts
@@ -7,11 +7,36 @@ import { Component } from '@angular/core';
 })
 export class InvestInStepsComponent {
   
-  openAccordionId:number | null = null
+openAccordionId: number | null = null;
 
-  toogleAccordion(id:number){
-   // this.isOpen= !this.isOpen
-   this.openAccordionId = this.openAccordionId === id ? null : id; // Toggle current accordion
+toogleAccordion(id: number) {
+  this.openAccordionId = this.openAccordionId === id ? null : id;
+}
 
-  }
+steps = [
+  {
+    id: 1,
+    title: 'Browse Live & Upcoming IPOs',
+    description:
+      'Get detailed insights into IPOs curated and presented through our platform.',
+  },
+  {
+    id: 2,
+    title: 'Generate Your Investor Number',
+    description:
+      'Start your IPO journey with ease by registering your National Investor Number.',
+  },
+  {
+    id: 3,
+    title: 'Make Your Investment',
+    description:
+      'Choose your desired IPO and place your investment directly through the platform.',
+  },
+  {
+    id: 4,
+    title: 'Monitor Your Portfolio',
+    description:
+      'Track your investments and portfolio performance in real time.',
+  },
+];
 }
